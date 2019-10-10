1d93fbef56ce190976de2b3e6c235eca1c04773c
yangbihua <193669519@qq.com>
2019-10-10 10:36:17 +0800

    初始化项目

---
+++ DouyuZhibo/DouyuZhibo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DouyuZhibo
+//
+//  Created by Billy on 2019/10/10.
+//  Copyright © 2019 Billy. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
